87bac8950d83a26c338538cd0c5755f128eb17d0
saalxx <saalxx@alumnatflorida.es>
2024-12-19 10:39:36 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,310 @@
+<?php
+
+$pomodoroHaters = [
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', 'A', '0', 'A', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', '0', '0', 'A', 'A', 'A', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '0', '0', '0', 'A', '0', 'A', 'A', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '~', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', 'A', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~'],
+    ['~', '0', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '0', '0', '0', 'A', '0', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', '0', 'A', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'A', 'A', 'A', '0', '0', 'A', '0', '0', '0', '~', '~', '~'],
+    ['~', '~', '~', '~', '0', 'A', 'A', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '0', 'A', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'A', '0', '0', '0', '0', '0', 'A', '0', 'A', '0', '0', '0', '~'],
+    ['~', '~', '~', '0', 'A', '0', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'A', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '0', '0', '0', '0', 'A', '0', '0', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~']
+];
+$impacts = [
+    [20, 4],
+    [2, 13],
+    [13, 12],
+    [3, 17],
+    [2, 13],
+    [5, 19],
+    [6, 18],
+    [5, 2],
+    [20, 13],
+    [9, 7],
+    [5, 9],
+    [15, 16],
+    [16, 13],
+    [16, 9],
+    [16, 0],
+    [3, 19],
+    [19, 8],
+    [1, 16],
+    [18, 4],
+    [21, 23],
+    [7, 17],
+    [22, 15],
+    [21, 6],
+    [14, 8],
+    [12, 23],
+    [7, 7],
+    [22, 4],
+    [3, 21],
+    [2, 3],
+    [8, 11],
+    [0, 4],
+    [7, 21],
+    [11, 4],
+    [7, 15],
+    [6, 17],
+    [5, 19],
+    [4, 19],
+    [4, 7],
+    [23, 21],
+    [15, 20],
+    [2, 9],
+    [21, 2],
+    [1, 13],
+    [7, 10],
+    [21, 5],
+    [13, 17],
+    [2, 14],
+    [11, 14],
+    [22, 17],
+    [18, 22],
+    [2, 23],
+    [3, 1],
+    [18, 6],
+    [17, 12],
+    [18, 18],
+    [20, 2],
+    [3, 14],
+    [11, 21],
+    [6, 5],
+    [6, 2],
+    [12, 23],
+    [18, 18],
+    [21, 6],
+    [10, 12],
+    [5, 4],
+    [16, 19],
+    [8, 10],
+    [12, 21],
+    [15, 1],
+    [20, 14],
+    [3, 20],
+    [6, 19],
+    [20, 13],
+    [15, 4],
+    [10, 2],
+    [5, 16],
+    [20, 1],
+    [12, 13],
+    [11, 5],
+    [12, 14],
+    [8, 3],
+    [6, 8],
+    [19, 7],
+    [16, 9],
+    [13, 20],
+    [3, 5],
+    [1, 0],
+    [20, 14],
+    [12, 21],
+    [12, 16],
+    [15, 7],    
+    [9, 1],
+    [1, 18],
+    [20, 6],
+    [8, 6],
+    [22, 1],
+    [10, 22],
+    [19, 19],
+    [7, 16],
+    [8, 8]
+];
+
+colores($pomodoroHaters);
+"<br>";
+echo "After impacts: <br>";
+colores(analyzeImpacts($pomodoroHaters, $impacts));
+echo "Cantidad de personas:  <br>";
+list($personas, $medicina) = catidadPer(analyzeImpacts($pomodoroHaters, $impacts));
+echo "Hay $personas en las zonas afectadas y  de medicina se necesita: $medicina";
+$analyzeImpacts = analyzeImpacts($pomodoroHaters, $impacts);
+$fullimpacts = mapSyX($analyzeImpacts, $impacts);
+echo "Mapa después de impactos de tierra: <br>";
+colores($fullimpacts);
+list ($Count, $count, $SumaTotales) = Limpieza($fullimpacts);
+echo "Limpieza de daños de zonas urbanas es de $count y de zonas no urbanas es de $Count y la suma de todo es de $SumaTotales <br>";
+$marGlobal=mar($fullimpacts);
+$marAfectado=marAfectado($fullimpacts);
+$pescadoONG=round(2000/$marGlobal*$marAfectado) ;
+$eurosONG=$pescadoONG*1000*5;
+echo "El total de pescado que disponen es de $pescadoONG , el mapa total es de $marGlobal y el mar afectado $marAfectado,<br> luego la ONG va a recaudar $eurosONG con la venta de pesado<br>";
+
+
+
+
+function ReadMap (array $map){
+    
+    for ($i = 0; $i < count ($map); $i++){
+        $count = 0;
+        for ($j = 0 ; $j <count ($map[$i]); $j++){
+            echo $map[$i][$j]. " ";
+            $count++;
+            if ($count == 24)
+                echo "<br>";    
+            
+        }
+    
+    }
+}
+
+
+function analyzeImpacts (array $map, array $impact){
+    
+    for ($i = 0; $i < count ($impact); $i++){
+        $impactx = null;
+        $impacty = null;
+        for ($j =  0; $j < count ($impact [$i]); $j++)
+        if ($j == 0){
+            $impactx = $impact[$i][$j];
+        }
+        else {$impacty = $impact[$i][$j];
+        } 
+        if  ($impactx != null && $impacty != null){
+            if ($map[$impactx][$impacty] == "A"){
+            $map[$impactx][$impacty] = "C";
+            }
+        }    
+    }
+    return $map;
+}
+
+function catidadPer (array $map): array {
+    $count = 0; 
+    $medicina = 0;   
+    for ($i = 0; $i < count($map); $i++){
+        for ($j = 0; $j < count($map[$i]); $j++){
+            if ($map[$i][$j] == "C"){
+                $count+= 5000;
+
+            }
+            
+        }
+    }
+    $medicina = $count * 0.025;
+    return array ($count, $medicina);
+}
+
+function mapSyX (array $map, array $impacts): array {
+
+    $impactx = 0;
+    $impacty = 0;
+
+    for ($i = 0; $i < count ($impacts); $i++) {
+        for ($j = 0; $j < count ($impacts[$i]); $j++) {
+            $impactx = $impacts[$i][0];
+            $impacty = $impacts[$i][1];
+            }   
+            if ($map[$impactx][$impacty] == "0"){
+                $map[$impactx][$impacty] = "X";
+            }
+            if ($map[$impactx][$impacty] == "~"){
+                $map[$impactx][$impacty] = "S";
+            }
+        }
+        return $map;
+    }
+
+
+function Limpieza (array $map): array {
+    $count = 0;
+    $Count = 0;
+    
+    for ($i = 0; $i < count ($map); $i++) {
+        for ($j = 0; $j < count ($map[$i]); $j++) {
+        if ($map[$i][$j] == "C"){
+            $count+= 200000;
+        }
+        if ($map[$i][$j] == "X"){
+            $Count+= 50000;
+        }
+    }  
+    $SumaTotales = ($count + $Count) ;}
+    return array ($Count, $count, $SumaTotales);
+ }
+
+function mar (array $map): int {
+    $marTotal = 0;
+
+    for ($i = 0; $i < count ($map); $i++) {
+        for ($j = 0; $j < count ($map[$i]); $j++){
+            if ($map[$i][$j] == "~" || $map[$i][$j] == "S") {
+                $marTotal += 1;
+            }
+            
+        }
+    }
+    return $marTotal;
+}
+
+function marAfectado (array $map): int {
+    $marTotal = 0;
+
+    for ($i = 0; $i < count ($map); $i++) {
+        for ($j = 0; $j < count ($map[$i]); $j++){
+            if ($map[$i][$j] == "S") {
+                $marTotal += 1;
+            }
+            
+        }
+    }
+    return $marTotal;
+}
+
+function colores(array $map) {
+    
+    for ($i = 0; $i < count ($map); $i++) {
+        $cont=0;
+        for ($j = 0; $j < count ($map[$i]); $j++){
+            if ($map[$i][$j] === "S"){
+                echo "<span style = 'background-color: red'>";
+                echo $map[$i][$j];
+                echo "</span>";
+            }
+            if ($map[$i][$j] === "~"){
+                echo "<span style = 'background-color: lightblue'>";
+                echo $map[$i][$j];
+                echo "</span>";
+            }
+            if ($map[$i][$j] === "C"){
+                echo "<span style = 'background-color: green'>";
+                echo $map[$i][$j];
+                echo "</span>";
+            }
+            if ($map[$i][$j] === "0"){
+                echo "<span style = 'background-color: blue'>";
+                echo $map[$i][$j];
+                echo "</span>";
+            }
+            if ($map[$i][$j] === "X"){
+                echo "<span style = 'background-color: yellow'>";
+                echo $map[$i][$j];
+                echo "</span>";
+            }
+        }
+        if ($cont == 0){
+            echo "<br>";
+        }
+    }
+    
+}
+
+ ?>   
